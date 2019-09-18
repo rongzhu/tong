a5813de87adead23d9b29fa91b5f43f33c32d360
Rong <raiserchu@yahoo.com>
2019-09-18 00:08:21 -0700

    fix chase checking parsing

--- Controllers/SandboxController.cs
+++ Controllers/SandboxController.cs
@@ -4,9 +4,17 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web;
+using System.Net.Mail;
 
 namespace tongbro.Controllers
 {
+	public class LogPostModel
+	{
+		public string keyword { get; set; }
+		public string queryid { get; set; }
+	}
+
     public class SandboxController : ApiController
     {
 		[HttpPost]
@@ -23,5 +31,25 @@ namespace tongbro.Controllers
 		{
 			return (string)System.Web.HttpContext.Current.Application["TempStash"];
 		}
+
+		[HttpPost]
+		[Route("api/103")]
+		public string LogPost(LogPostModel model)
+		{
+			var req = HttpContext.Current.Request;
+			MailMessage message = new MailMessage("admin@tongbrothers.com", "postmaster@tongbrothers.com", "Log Post",
+				string.Format("View attempted: {0} from {1} (query {2}) referer {3} at UTC {4}", model.keyword, req.UserHostAddress,
+				model.queryid, req.UrlReferrer, DateTime.UtcNow.ToString()));
+
+			SmtpClient client = new SmtpClient("mail.tongbrothers.com");
+			int n = 2222;
+			NetworkCredential Credentials = new NetworkCredential("postmaster@tongbrothers.com", "winhost" + (n + 1122));
+			client.Credentials = Credentials;
+			client.Send(message);
+
+			HttpContext.Current.Response.Redirect("/content/messagepage.html");
+			return "";
+		}
+
 	}
 }
--- ng/app.js
+++ ng/app.js
@@ -73,7 +73,10 @@ angular.module('expApp', [
 	};
 })
 .run(['$rootScope', '$location', function ($rootScope, $location) {
-	$rootScope.expenseCategories = { "1": "Household", "2": "Gas", "3": "Entertainment", "4": "Durable", "5": "Vacation", "6": "TutorTime", "7": "Toll", "8": "Periodical" };
+	$rootScope.expenseCategories = {
+		"1": "Household", "2": "Gas", "3": "Entertainment", "4": "Durable", "5": "Vacation", "6": "TutorTime", "7": "Toll", "8": "Periodical",
+		"9": "Project 1", "10": "Project 2"
+	};
 	$rootScope.getMonthSum = function () {
 		return $location.path().substr(0, 9) === '/monthly/' ? $rootScope.monthSum : null;
 	}
